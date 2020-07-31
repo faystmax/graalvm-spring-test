8812c1cd8d37dc609027191891b606e62af986ab
Amosov Maxim <faystmax@gmail.com>
2020-07-31 21:34:17 +0700

    Rename controller

--- src/main/java/com/faystmax/graalvm/web/HelloGraalVMController.java
+++ src/main/java/com/faystmax/graalvm/web/HelloGraalVMController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class HelloGraalVMController {
     @GetMapping("/")
     public String hello() {
         return "Hello from GraalVM";
